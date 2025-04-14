ea092bf65e70a1850b182c3372e37c3fe436abe2
xhm--hl <2898522927@qq.com>
2025-04-14 13:39:41 +0800

    git commit -am "修改问候语为Everyone"

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ def greet(name):
     print(f"Hello, {name}!")
 def goodbye(name):
     print(f"Goodbye, {name}!")
-greet("GitHub")
 goodbye("GitHub")
- def greet(name):
- print(f"Hello, {name}!")
- greet("GitHub")
+greet("Everyone")
